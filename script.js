1b031c1e49a2cdc33c12c3fa1b86dc9d7e9ea6ae
Uno-B <unumandakh.b@gmail.com>
2018-11-01 10:35:49 +0300

    Added end game box

---
+++ script.js
@@ -0,0 +1,15 @@
+var origBoard;
+const huPlayer = 'O';
+const aiPlayer = '';
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
