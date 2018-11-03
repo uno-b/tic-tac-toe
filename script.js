01f10a48f1f8d20c29c50728c2c3d4420a1afe2d
Uno-B <unumandakh.b@gmail.com>
2018-11-03 21:23:02 +0300

    Added event listener

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ const winCombos = [
 ]
 
 const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = "";
+		cells[i].style.removeProperty("background-color");
+		cells[i].addEventListener("click", turnClick, false);
+	}
+}
+
+function turnClick(square) {
+	turn(square.target.id, huPlayer);
+}
+
+function turn(squareId, player) {
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+}
+
+
+
+
+
+
+
+
