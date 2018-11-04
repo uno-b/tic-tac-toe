5023ff9963a6cdad4835387ee92cf9ff7dd887a9
Uno-B <unumandakh.b@gmail.com>
2018-11-04 22:02:16 +0300

    Minimax algorithm in progress

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var origBoard;
-const huPlayer = 'O';
-const aiPlayer = '';
+const huPlayer = "O";
+const aiPlayer = "X";
 const winCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -78,7 +78,7 @@ function emptySquares() {
 }
 
 function bestSpot() {
-	return emptySquares()[0];
+	return minimax(origBoard, aiPlayer).index;
 }
 
 function checkTie() {
@@ -91,6 +91,44 @@ function checkTie() {
 		return true;
 	}
 	return false;
+}
+
+function minimax(newBoard, player) {
+	var availSpots = emptySquares(newBoard);
+
+	if (checkWin(newBoard, player)) {
+		return { score: -10; }
+	} else if (checkWin(newBoard, aiPlayer)) {
+		return { score: 20; }
+	} else if (availSpots.length === 0) {
+		return { score: 0; }
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = newBoard[availSpots[i]];
+		newBoard[availSpots[i]] = player;
+
+		if (player == aiPlayer) {
+			var result = minimax(newBoard, huPlayer);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, aiPlayer);
+			move.score = result.score;
+		}
+
+		newBoard[availSpots[i]] = move.index;
+
+		moves.push(move);
+	}
+
+	var bestMove;
+	if (player == aiPlayer) {
+		var bestScore = -10000;
+		for (var i = 0; i < moves.length; i++) {
+			
+		}
+	}
 }
 
 
