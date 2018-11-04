9e75989c0deaf62c5bb05123f531258fae158df3
Uno-B <unumandakh.b@gmail.com>
2018-11-04 21:47:45 +0300

    Wrote function checkTie

--- script.js
+++ script.js
@@ -81,7 +81,17 @@ function bestSpot() {
 	return emptySquares()[0];
 }
 
-
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for(var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener("click", turnClick, false);
+		}
+		declareWinner("Tie Game!");
+		return true;
+	}
+	return false;
+}
 
 
 
